cdc31ab3a36b15d0a7c1744fe4783672eb1fb4c5
ullasbc <ullasbc02@gmail.com>
2026-04-28 14:14:03 -0400

    functions done

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me(){
+    println!("hello");
+}
 fn main() {
     call_me(); // Don't change this line
 }
